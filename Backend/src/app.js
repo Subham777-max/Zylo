f7257a9e71c44eecbfa49cb674969afba6b860aa
Subham777-max <subhamray865@gmail.com>
2026-04-10 23:02:44 +0530

    config file update

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,8 +3,9 @@ import morgan from "morgan";
 import cookieParser from "cookie-parser";
 const app = express();
 
-app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 
