62682b720792ea77acf82aca56301c0a3ec2c836
Subham777-max <subhamray865@gmail.com>
2026-04-12 21:06:32 +0530

    feat: add routing with react-router-dom and create initial routes

--- Frontend/src/app/App.jsx
+++ Frontend/src/app/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './App.routes'
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
+      <RouterProvider router={router} />
     </>
   )
 }
