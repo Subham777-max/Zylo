6d7fad0105c9db9c78fd9a6fb93316e805efc782
Subham777-max <subhamray865@gmail.com>
2026-04-13 19:50:38 +0530

    fix: update CORS middleware configuration and adjust API base URL for consistency

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,10 +6,10 @@ import cors from "cors";
 const app = express();
 
 // Middlewares
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-}));
+// app.use(cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+// }));
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
